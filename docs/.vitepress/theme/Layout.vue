5994a28b8c6b1173646602592cc90514e3b7da1b
huhai <2075313210@qq.com>
2026-01-28 16:18:50 +0800

    添加极光背景组件

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 import { Content } from 'vitepress';
+import RecordBackground from './components/RecordBackground.vue';
 </script>
 
 <template>
-	<div class="w-full">
-		<Content />
+	<div class="relative w-full">
+		<ClientOnly>
+			<RecordBackground class-name="w-full h-[100dvh] fixed inset-0 z-[-1]" />
+		</ClientOnly>
+		<div class="flex h-full min-h-screen w-full flex-col">
+			<Content />
+		</div>
 	</div>
 </template>
 
