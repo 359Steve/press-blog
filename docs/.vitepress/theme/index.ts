36ed803c184058d2a373c2ccecbb058ef9f72a71
huhai <2075313210@qq.com>
2026-01-31 21:33:22 +0800

    全局注册组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,26 @@
 import type { Theme } from 'vitepress';
+import { Icon } from '@iconify/vue';
+import { createPinia } from 'pinia';
+import HeaderMenuList from './components/HeaderMenuList.vue';
+import LogoBasicLogo from './components/LogoBasicLogo.vue';
+import MainPress from './components/MainPress.vue';
+import NodetFound from './components/NotFound.vue';
+import RecordBackground from './components/RecordBackground.vue';
+import TRexRunner from './components/TRexRunner.vue';
 import Layout from './Layout.vue';
+import 'vitepress/theme';
 import './style.css';
 
 export default {
 	Layout,
-	enhanceApp({ app, router, siteData }) {
-
+	enhanceApp({ app }) {
+		app.use(createPinia());
+		app.component('Icon', Icon)
+			.component('RecordBackground', RecordBackground)
+			.component('MainPress', MainPress)
+			.component('NotFound', NodetFound)
+			.component('TRexRunner', TRexRunner)
+			.component('LogoBasicLogo', LogoBasicLogo)
+			.component('HeaderMenuList', HeaderMenuList);
 	},
 } satisfies Theme;
