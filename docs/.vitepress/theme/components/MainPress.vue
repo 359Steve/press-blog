0a7bf503a47be05a40faf16f0f485a6fe0c27866
huhai <2075313210@qq.com>
2026-01-31 21:36:22 +0800

    创建主区域组件

---
+++ docs/.vitepress/theme/components/MainPress.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+	<main class="box-border min-h-screen">
+		<slot name="header" />
+		<div class="vp-doc m-auto w-full max-w-(--prose-width) px-6 py-2">
+			<slot />
+		</div>
+	</main>
+</template>
+
+<style lang="scss" scoped></style>
