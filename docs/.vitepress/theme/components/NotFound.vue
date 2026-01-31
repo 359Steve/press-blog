5d5e770ab73f7f7104513acaa1d3fc74eae3ae6a
huhai <2075313210@qq.com>
2026-01-31 21:36:30 +0800

    创建404页面

---
+++ docs/.vitepress/theme/components/NotFound.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="flex h-screen w-full flex-col justify-center text-center">
+		<div
+			class="h-62.5 w-full bg-[url(/images/404-ezgif.com-gif-maker.gif)] bg-contain bg-center bg-no-repeat sm:h-87.5 md:h-100"
+		>
+			<span class="text-center text-6xl sm:text-7xl md:text-8xl">404</span>
+		</div>
+
+		<div class="-mt-12.5 w-full">
+			<span class="mb-4 text-2xl font-bold sm:text-3xl">哎呀，页面跑丢了！</span>
+			<p class="mb-6 sm:mb-5">这个页面可能去度假了，或者是被藏进了山里！</p>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
