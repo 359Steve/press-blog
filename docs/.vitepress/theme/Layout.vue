03476ba60377394d30d83b9b51fa78e084dacc3e
huhai <2075313210@qq.com>
2026-02-02 18:16:47 +0800

    定义切换主题方法

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -2,6 +2,37 @@
 const { page, isDark } = useData();
 const { setScroll, getHeaderHeight } = useJojoHeader();
 
+function enableTransitions() {
+	return 'startViewTransition' in document && window.matchMedia('(prefers-reduced-motion: no-preference)').matches;
+}
+
+provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+	if (!enableTransitions()) {
+		isDark.value = !isDark.value;
+		return;
+	}
+
+	const clipPath = [
+		`circle(0px at ${x}px ${y}px)`,
+		`circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`,
+	];
+
+	await document.startViewTransition(async () => {
+		isDark.value = !isDark.value;
+		await nextTick();
+	}).ready;
+
+	document.documentElement.animate(
+		{ clipPath: isDark.value ? clipPath.reverse() : clipPath },
+		{
+			duration: 500,
+			easing: 'ease-in',
+			fill: 'forwards',
+			pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+		},
+	);
+});
+
 onMounted(() => {
 	const { y } = useWindowScroll();
 
