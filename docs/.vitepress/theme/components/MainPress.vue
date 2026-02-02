a79bc98b01a5590d171fe6bdf1f5ecb59cc3ea90
huhai <2075313210@qq.com>
2026-02-02 18:17:27 +0800

    修改主体组件布局

--- docs/.vitepress/theme/components/MainPress.vue
+++ docs/.vitepress/theme/components/MainPress.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-	<main class="box-border min-h-screen">
+	<main class="box-border flex! min-h-screen flex-col">
 		<slot name="header" />
-		<div class="vp-doc m-auto w-full max-w-(--prose-width) px-6 py-2">
+		<div class="vp-doc m-auto w-full max-w-(--prose-width) flex-1 px-6 py-2">
 			<slot />
 		</div>
+		<slot name="footer" />
 	</main>
 </template>
 
