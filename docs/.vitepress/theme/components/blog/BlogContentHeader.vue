8e1b636ebcd0cb7cdd065448bd885c9ce455fde4
huhai <2075313210@qq.com>
2026-02-03 19:00:26 +0800

    创建博客统一显示页头

---
+++ docs/.vitepress/theme/components/blog/BlogContentHeader.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const { title, date } = defineProps<{
+	title: string;
+	date: string;
+}>();
+
+const transformDate = computed(() => {
+	const newDate = new Date(date);
+	newDate.setUTCHours(12);
+	return newDate.toLocaleDateString('zh-CN', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	});
+});
+</script>
+
+<template>
+	<h1 class="text-3xl font-bold">
+		{{ title }}
+	</h1>
+	<p class="text-blog-tertiary my-2">发布于 {{ transformDate }}</p>
+	<br />
+</template>
+
+<style lang="scss" scoped></style>
