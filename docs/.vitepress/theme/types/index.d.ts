e7d31fc827c8e3bc4f47b7e56c4c7e8881dcce78
huhai <2075313210@qq.com>
2026-02-03 19:01:50 +0800

    添加博客索引类型

--- docs/.vitepress/theme/types/index.d.ts
+++ docs/.vitepress/theme/types/index.d.ts
@@ -14,3 +14,28 @@ interface ThemeVavbarItem {
 interface ThemeNavbarConfig extends ThemeVavbarItem {
 	icon: string;
 }
+
+/**
+ * @description 博客索引类型
+ */
+interface Post {
+	title: string;
+	cover: string;
+	description: string;
+	date: {
+		time: number;
+		string: string;
+	};
+	author: string;
+	tags: {
+		name: string;
+		color: string;
+		icon: string;
+		url: string;
+	}[];
+	// 默认类型
+	url: string | undefined;
+	src: string | undefined;
+	html: string | undefined;
+	excerpt: string | undefined;
+}
