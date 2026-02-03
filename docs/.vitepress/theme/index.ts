6de3f62f3000de8239a7fa99f79a330750393546
huhai <2075313210@qq.com>
2026-02-03 18:58:35 +0800

    删除多余组件的全局注册

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,8 @@
 import type { Theme } from 'vitepress';
 import { Icon } from '@iconify/vue';
 import { createPinia } from 'pinia';
+import BlogList from './components/blog/BlogList.vue';
 import BlogSearch from './components/blog/BlogSearch.vue';
-import FooterBox from './components/footer/FooterBox.vue';
-import HeaderBox from './components/header/HeaderBox.vue';
-import HeaderItem from './components/header/HeaderItem.vue';
-import HeaderMenuList from './components/header/HeaderMenuList.vue';
-import LogoBasicLogo from './components/header/LogoBasicLogo.vue';
-import MainPress from './components/MainPress.vue';
-import NodetFound from './components/NotFound.vue';
-import RecordBackground from './components/RecordBackground.vue';
-import TRexRunner from './components/TRexRunner.vue';
 import Layout from './Layout.vue';
 import 'vitepress/theme';
 import './style.css';
@@ -19,16 +11,6 @@ export default {
 	Layout,
 	enhanceApp({ app }) {
 		app.use(createPinia());
-		app.component('Icon', Icon)
-			.component('RecordBackground', RecordBackground)
-			.component('MainPress', MainPress)
-			.component('NotFound', NodetFound)
-			.component('TRexRunner', TRexRunner)
-			.component('LogoBasicLogo', LogoBasicLogo)
-			.component('HeaderBox', HeaderBox)
-			.component('HeaderItem', HeaderItem)
-			.component('HeaderMenuList', HeaderMenuList)
-			.component('FooterBox', FooterBox)
-			.component('BlogSearch', BlogSearch);
+		app.component('Icon', Icon).component('BlogSearch', BlogSearch).component('BlogList', BlogList);
 	},
 } satisfies Theme;
