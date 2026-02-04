e19da467522965b035e65cc40f69c41eb079a36c
huhai <2075313210@qq.com>
2026-02-04 14:44:04 +0800

    删除多余全局注册组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,6 @@
 import type { Theme } from 'vitepress';
 import { Icon } from '@iconify/vue';
 import { createPinia } from 'pinia';
-import BlogList from './components/blog/BlogList.vue';
-import BlogSearch from './components/blog/BlogSearch.vue';
 import Layout from './Layout.vue';
 import 'vitepress/theme';
 import '@/theme/css/style.css';
@@ -11,6 +9,6 @@ export default {
 	Layout,
 	enhanceApp({ app }) {
 		app.use(createPinia());
-		app.component('Icon', Icon).component('BlogSearch', BlogSearch).component('BlogList', BlogList);
+		app.component('Icon', Icon);
 	},
 } satisfies Theme;
