b6d980508bc94eaaf3b3561ffc4f2d5461ca6666
huhai <2075313210@qq.com>
2026-02-04 16:10:29 +0800

    修改全局注册组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress';
 import { Icon } from '@iconify/vue';
 import { createPinia } from 'pinia';
+import DocTable from './components/DocTable.vue';
 import Layout from './Layout.vue';
 import 'vitepress/theme';
 import '@/theme/css/style.css';
@@ -9,6 +10,6 @@ export default {
 	Layout,
 	enhanceApp({ app }) {
 		app.use(createPinia());
-		app.component('Icon', Icon);
+		app.component('Icon', Icon).component('DocTable', DocTable);
 	},
 } satisfies Theme;
