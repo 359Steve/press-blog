483aaac1a97ea90dc7cd9fef1cc530d9f13068b8
huhai <2075313210@qq.com>
2026-02-04 16:11:03 +0800

    添加博客详情返回按钮

--- docs/.vitepress/theme/components/layouts/BlogListLayout.vue
+++ docs/.vitepress/theme/components/layouts/BlogListLayout.vue
@@ -2,6 +2,7 @@
 import { transformDate } from '@/theme/utils';
 
 const { frontmatter } = useData();
+const router = useRouter();
 
 const getDate = computed(() => {
 	return transformDate(new Date(frontmatter.value.date));
@@ -10,12 +11,16 @@ const getDate = computed(() => {
 
 <template>
 	<div class="mt-6 w-full pt-10 sm:mt-10">
-		<h1 class="text-3xl font-bold">
+		<h1 class="text-3xl font-semibold">
 			{{ frontmatter.title }}
 		</h1>
 		<p class="text-blog-tertiary my-2">发布于 {{ getDate.string }}</p>
 		<br />
 		<Content />
+		<div class="text-blog-tertiary mt-10 flex w-fit items-center gap-1 text-lg" @click="router.go('/blog')">
+			<Icon icon="mdi:chevron-right" width="24" />
+			<span class="cursor-pointer border-b border-gray-400">cd . .</span>
+		</div>
 	</div>
 </template>
 
