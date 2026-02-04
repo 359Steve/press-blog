0901b19fc126e458072bf2be9b18337a55ba9947
huhai <2075313210@qq.com>
2026-02-04 14:44:59 +0800

    移动全局布局组件位置

---
+++ docs/.vitepress/theme/components/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const isNotFound = computed(() => route.data.title === '404');
+</script>
+
+<template>
+	<main class="box-border flex! min-h-screen flex-col">
+		<slot name="header" />
+		<div
+			class="vp-doc m-auto w-full max-w-(--prose-width) flex-1 px-6 py-2"
+			:class="isNotFound && 'flex items-center justify-center'"
+		>
+			<slot />
+		</div>
+		<slot name="footer" />
+	</main>
+</template>
+
+<style lang="scss" scoped></style>
