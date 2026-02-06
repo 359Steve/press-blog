a1b355be62a4f8247ce8361e2bf060d3b6b81fae
huhai <2075313210@qq.com>
2026-02-06 16:33:23 +0800

    修改博客文章首页布局组件

--- docs/.vitepress/theme/components/layouts/BlogIndexLayout.vue
+++ docs/.vitepress/theme/components/layouts/BlogIndexLayout.vue
@@ -1,15 +1,10 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-
-const search = ref<string>('');
-const debouncedSearch = useDebounce(search, 300);
+const { frontmatter } = useData();
 </script>
 
 <template>
-	<div class="mt-6 w-full sm:mt-10">
-		<BlogSearch v-model:search="search" />
-		<BlogList :search="debouncedSearch" />
-	</div>
+	<BlogListLayout v-if="frontmatter.blog" />
+	<BlogSearchLayout v-else />
 </template>
 
 <style lang="scss" scoped></style>
