59633e12513ef386dcad82fdfe0a411be3f7e7bd
huhai <2075313210@qq.com>
2026-02-06 16:32:32 +0800

    注册指令

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,8 @@
 import type { Theme } from 'vitepress';
 import { Icon } from '@iconify/vue';
 import { createPinia } from 'pinia';
+import { preview } from '@/theme/directives/preview';
+import { unwrap } from '@/theme/directives/unwrap';
 import TocList from './components/blog/TocList.vue';
 import DocTable from './components/DocTable.vue';
 import ContentImage from './components/md/ContentImage.vue';
@@ -12,6 +14,7 @@ export default {
 	Layout,
 	enhanceApp({ app }) {
 		app.use(createPinia());
+		app.directive('unwrap', unwrap).directive('preview', preview);
 		app.component('Icon', Icon)
 			.component('DocTable', DocTable)
 			.component('TocList', TocList)
