a6edcf0cbb97fd2b933190ce7a9d2ef3dd2bc19d
huhai <2075313210@qq.com>
2026-02-06 16:36:24 +0800

    创建去掉md默认p标签指令

---
+++ docs/.vitepress/theme/directives/unwrap.ts
@@ -0,0 +1,39 @@
+import type { Directive } from 'vue';
+
+type UnwrapPredicate = (el: HTMLElement) => boolean;
+
+type UnwrapValue = UnwrapPredicate | boolean | undefined;
+
+function runUnwrap(el: HTMLElement, tag: string, predicate: UnwrapValue): void {
+	const nodes = el.querySelectorAll<HTMLElement>(tag);
+
+	nodes.forEach((node) => {
+		let shouldUnwrap = true;
+
+		if (typeof predicate === 'function') {
+			shouldUnwrap = predicate(node);
+		} else if (predicate === false) {
+			shouldUnwrap = false;
+		}
+
+		if (!shouldUnwrap) {
+			return;
+		}
+
+		node.replaceWith(...node.childNodes);
+	});
+}
+
+export const unwrap: Directive<HTMLElement, UnwrapValue> = {
+	mounted(el, binding) {
+		const tag = binding.arg || 'p';
+		const predicate = binding.value;
+		queueMicrotask(() => runUnwrap(el, tag, predicate));
+	},
+
+	updated(el, binding) {
+		const tag = binding.arg || 'p';
+		const predicate = binding.value;
+		queueMicrotask(() => runUnwrap(el, tag, predicate));
+	},
+};
