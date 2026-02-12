39ebdd17aac9430e7aaa75700a153e7fa7aa24a0
huhai <2075313210@qq.com>
2026-02-12 16:55:48 +0800

    修改公共类型

--- docs/.vitepress/theme/types/index.d.ts
+++ docs/.vitepress/theme/types/index.d.ts
@@ -1,21 +1,4 @@
 /**
- * @description 菜单项类型
- */
-interface ThemeVavbarItem {
-	text: string;
-	link: string;
-	activeMatch?: string;
-	[key: string]: any;
-}
-
-/**
- * @description 顶部导航菜单列表类型
- */
-interface ThemeNavbarConfig extends ThemeVavbarItem {
-	icon: string;
-}
-
-/**
  * @description 博客索引类型
  */
 interface Post {
@@ -40,7 +23,9 @@ interface Post {
 	excerpt: string | undefined;
 }
 
-// 图片类型
+/**
+ * @description 相册图片类型
+ */
 interface ImageType {
 	src: string;
 	alt: string;
