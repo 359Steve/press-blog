27d55ad6e0fdfb713a50dca52346a76cea0fc21b
huhai <2075313210@qq.com>
2026-02-12 17:31:41 +0800

    修改aside样式

--- docs/.vitepress/theme/components/aside/AsideBox.vue
+++ docs/.vitepress/theme/components/aside/AsideBox.vue
@@ -21,10 +21,10 @@ function toPath(path: string): void {
 <template>
 	<div v-if="showSidebar" class="fixed inset-0 z-40 bg-black/40 lg:hidden" @click="setShowSidebar(false)" />
 	<aside
-		class="bg-bg-blog-primary fixed inset-y-0 left-0 z-50 h-full w-60 transform p-4 shadow-lg transition-transform lg:static lg:w-52 lg:translate-x-0 lg:bg-inherit lg:p-0 lg:shadow-none"
+		class="bg-bg-blog-primary scroll-y-hidden fixed inset-y-0 left-0 z-50 h-full w-64 transform px-2 transition-transform lg:static lg:w-60 lg:translate-x-0 lg:bg-inherit lg:px-0"
 		:class="showSidebar ? 'translate-x-0' : '-translate-x-full'"
 	>
-		<div class="sticky top-6 flex flex-wrap justify-center gap-6">
+		<div class="sticky top-6 flex flex-wrap justify-center gap-6 px-4 pb-3">
 			<AsideAvatar :logo="theme.logo" :logo-link="theme.logoLink" @to-path="toPath" />
 
 			<AsideCount />
