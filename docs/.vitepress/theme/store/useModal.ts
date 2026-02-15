b91c33a002a362a8c7fc6a540311b85539d85895
huhai <2075313210@qq.com>
2026-02-15 15:09:44 +0800

    创建弹窗公共状态管理

---
+++ docs/.vitepress/theme/store/useModal.ts
@@ -0,0 +1,34 @@
+export const useModals = defineStore('useModals', () => {
+	type ModalName = keyof typeof currentComponent;
+	type ModalProps<K extends ModalName> = PropsOf<(typeof currentComponent)[K]>;
+	type AllModalProps = {
+		[K in ModalName]: ModalProps<K>;
+	}[ModalName];
+
+	const visible = ref<boolean>(false);
+	const component = shallowRef<Component | null>(null);
+	const props = ref<AllModalProps>();
+
+	const currentComponent = {
+		ImageMask: defineAsyncComponent(() => import('@/theme/components/modal/ImageMask.vue')),
+		SearchMask: defineAsyncComponent(() => import('@/theme/components/modal/SearchMask.vue')),
+	};
+
+	function open<K extends ModalName>(comp: K, p?: ModalProps<K>) {
+		component.value = currentComponent[comp];
+		props.value = p;
+		visible.value = true;
+	}
+
+	function close() {
+		visible.value = false;
+	}
+
+	return {
+		visible,
+		component,
+		props,
+		open,
+		close,
+	};
+});
