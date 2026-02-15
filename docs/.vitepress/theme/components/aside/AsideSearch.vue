f3daf47650f53cc7211b025695fb0c9b73ae798b
huhai <2075313210@qq.com>
2026-02-15 15:06:06 +0800

    搜索按钮添加代开弹窗逻辑

--- docs/.vitepress/theme/components/aside/AsideSearch.vue
+++ docs/.vitepress/theme/components/aside/AsideSearch.vue
@@ -1,8 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { open } = useModals();
+</script>
 
 <template>
 	<div class="glass w-full rounded-lg p-3 shadow-[0_0_20px_var(--color-blog-shadow)]">
-		<input type="text" placeholder="搜索" class="text-base" placeholder-class="input-p" />
+		<button
+			type="button"
+			class="text-blog-tertiary! h-full w-full text-left text-base"
+			aria-label="搜索"
+			data-v-dc969bf6=""
+			data-v-76d3335c=""
+			@click="open('SearchMask')"
+		>
+			<span class="vpi-search" aria-hidden="true" data-v-76d3335c="" />
+			<span class="text" data-v-76d3335c="">搜索</span>
+		</button>
 	</div>
 </template>
 
