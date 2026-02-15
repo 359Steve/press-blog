e8546b9495ad9ca9916bc5458a4116dc81927b94
huhai <2075313210@qq.com>
2026-02-15 15:08:17 +0800

    创建弹窗入口组件

---
+++ docs/.vitepress/theme/components/modal/ModalHost.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const modal = useModals();
+const { close } = modal;
+const { visible, component, props } = storeToRefs(modal);
+</script>
+
+<template>
+	<MaskBox :visible @close="close">
+		<component :is="component" v-bind="props" @close="close" />
+	</MaskBox>
+</template>
