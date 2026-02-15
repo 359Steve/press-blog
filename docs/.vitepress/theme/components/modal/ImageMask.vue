5c8e8d1953b26461d06395a5f429f1c9b9c8a7ca
huhai <2075313210@qq.com>
2026-02-15 15:07:49 +0800

    创建预览图片组件

---
+++ docs/.vitepress/theme/components/modal/ImageMask.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+// import * as livephotoskit from 'livephotoskit';
+// const { augmentElementAsPlayer } = livephotoskit;
+
+defineProps<{
+	src: string;
+	alt: string;
+}>();
+// const livePhotoRef = useTemplateRef<HTMLDivElement>('livePhotoRef');
+
+// watch(
+// 	() => getPreviewVisible(),
+// 	(visible) => {
+// 		if (!visible) return;
+
+// 		nextTick(() => {
+// 			const el = livePhotoRef.value;
+// 			if (!el) return;
+
+// 			augmentElementAsPlayer(el, {
+// 				photoSrc: getPreviewSrc(),
+// 				videoSrc: getIsLive(),
+// 			});
+
+// 			document.querySelector('.lpk-badge')?.addEventListener('click', (e) => e.stopImmediatePropagation(), true);
+// 		});
+// 	},
+// );
+</script>
+
+<template>
+	<img
+		:src
+		:alt
+		loading="lazy"
+		decoding="async"
+		class="z-2 h-full max-h-screen w-full max-w-[100vw] object-contain"
+	/>
+</template>
+
+<style lang="postcss" scoped></style>
