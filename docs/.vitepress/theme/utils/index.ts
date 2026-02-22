b08502586fbbbf8f8683baff9257cf166993b1e7
huhai <2075313210@qq.com>
2026-02-22 17:50:49 +0800

    创建统计数据通用方法

--- docs/.vitepress/theme/utils/index.ts
+++ docs/.vitepress/theme/utils/index.ts
@@ -12,3 +12,29 @@ export function transformDate(raw: Date): Post['date'] {
 		}),
 	};
 }
+
+/**
+ * 统计数据
+ */
+export function stats(posts: Post[], labelName: string) {
+	const allPosts = posts ?? [];
+	const allTags = new Set<string>();
+	const years = new Set<number>();
+	const categorys = new Set<string>();
+
+	allPosts.forEach((post) => {
+		post.tags?.forEach((tag) => allTags.add(tag.name));
+		years.add(new Date(post.date.time).getFullYear());
+		categorys.add(post.category);
+	});
+
+	return {
+		totalPosts: allPosts.length,
+		totalTags: allTags.size,
+		totalYears: years.size,
+		category: categorys.size,
+		filteredPosts: labelName
+			? groupedPosts(posts, labelName).reduce((sum, group) => sum + group.posts.length, 0)
+			: allPosts.length,
+	};
+}
