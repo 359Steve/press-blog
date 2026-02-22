1c407bb32ea5e55e19b3f1f9bf308cba7ce76c4b
huhai <2075313210@qq.com>
2026-02-22 17:51:05 +0800

    创建获取月份名称方法

--- docs/.vitepress/theme/utils/index.ts
+++ docs/.vitepress/theme/utils/index.ts
@@ -14,6 +14,14 @@ export function transformDate(raw: Date): Post['date'] {
 }
 
 /**
+ * 获取月份名称
+ */
+export function getMonthName(month: number) {
+	const months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+	return months[month - 1];
+}
+
+/**
  * 统计数据
  */
 export function stats(posts: Post[], labelName: string) {
