26973d0d9c9e1f303d18320edb3cf7205e394920
huhai <2075313210@qq.com>
2026-02-24 21:53:29 +0800

    解决水合问题

--- docs/.vitepress/theme/components/aside/AsideMenu.vue
+++ docs/.vitepress/theme/components/aside/AsideMenu.vue
@@ -7,14 +7,14 @@ const { menuList } = defineProps<{
 defineEmits<{
 	(e: 'toPath', value: string): void;
 }>();
-const route = useRoute();
+// const route = useRoute();
 
-function isActive(item: NavItemWithIcon): boolean {
-	if (!item.activeMatch) {
-		return false;
-	}
-	return new RegExp(item.activeMatch).test(route.path);
-}
+// function isActive(item: NavItemWithIcon): boolean {
+// 	if (!item.activeMatch) {
+// 		return false;
+// 	}
+// 	return new RegExp(item.activeMatch).test(route.path);
+// }
 </script>
 
 <template>
@@ -24,11 +24,6 @@ function isActive(item: NavItemWithIcon): boolean {
 				v-for="item in menuList"
 				:key="item.link"
 				class="flex h-full w-full items-center justify-center gap-4 px-0 text-center text-base leading-10 hover:cursor-pointer"
-				:class="[
-					isActive(item)
-						? 'bg-blog-accent rounded-lg text-white shadow-[0_0_10px_var(--color-blog-accent)]'
-						: 'text-blog-tertiary bg-inherit',
-				]"
 				@click="$emit('toPath', item.link)"
 			>
 				<Icon :icon="item.icon" width="18" />
