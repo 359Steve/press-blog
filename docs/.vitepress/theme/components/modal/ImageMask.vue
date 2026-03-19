171035354424fc113474f747d046797d31b76905
huhai <2075313210@qq.com>
2026-03-19 16:05:36 +0800

    添加实况功能

--- docs/.vitepress/theme/components/modal/ImageMask.vue
+++ docs/.vitepress/theme/components/modal/ImageMask.vue
@@ -1,12 +1,50 @@
 <script lang="ts" setup>
-defineProps<{
+import * as livephotoskit from 'livephotoskit';
+
+const { src, alt, live } = defineProps<{
 	src: string;
 	alt: string;
+	live?: Photo['live'];
 }>();
+
+const { augmentElementAsPlayer } = livephotoskit;
+const livePhotoRef = useTemplateRef<HTMLDivElement>('livePhotoRef');
+const btn = ref<Element | null>(null);
+
+function handleClick(e: Event) {
+	e.stopImmediatePropagation();
+}
+
+watchEffect(() => {
+	nextTick(() => {
+		if (!live) return;
+		const el = livePhotoRef.value;
+		if (!el) return;
+
+		augmentElementAsPlayer(el, {
+			photoSrc: src,
+			videoSrc: live?.url,
+		});
+
+		const badge = document.querySelector('.lpk-badge');
+		if (!badge) return;
+
+		badge.removeEventListener('click', handleClick, true);
+		badge.addEventListener('click', handleClick, true);
+
+		btn.value = badge;
+	});
+});
+
+onBeforeUnmount(() => {
+	btn.value?.removeEventListener('click', handleClick, true);
+});
 </script>
 
 <template>
+	<div v-if="live" ref="livePhotoRef" class="h-full w-full" />
 	<img
+		v-else
 		:src
 		:alt
 		loading="lazy"
